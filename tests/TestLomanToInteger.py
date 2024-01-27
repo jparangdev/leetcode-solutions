a8494483a49489c8331c22a7e72e20e4ca604f3c
jparangdev <jparangdev@gmail.com>
2024-01-27 22:24:15 +0900

    LomanToInteger.py

---
+++ tests/TestLomanToInteger.py
@@ -0,0 +1,20 @@
+import unittest
+
+from solutions.LomanToInteger import LomanToInteger
+
+
+class TestLomanToInteger(unittest.TestCase):
+    def setUp(self):
+        self.instance = LomanToInteger()
+
+    def test(self):
+        cases = [
+            ("III", 3),
+            ("LVIII", 58),
+            ("MCMXCIV", 1994),
+            # Add more test cases if needed
+        ]
+        for case, expected in cases:
+            with self.subTest(case=case):
+                self.assertEqual(self.instance.romanToInt(case), expected)
+
