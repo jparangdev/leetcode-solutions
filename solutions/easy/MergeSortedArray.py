363d7972debaffb688827cd9b2749e395d3faf0b
jparangdev <jparangdev@gmail.com>
2024-03-01 22:04:06 +0900

    MergeSortedArray.py

--- solutions/easy/MergeSortedArray.py
+++ solutions/easy/MergeSortedArray.py
@@ -1,4 +1,7 @@
-class MergeSortedArray
+from typing import List
+
+
+class MergeSortedArray:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         nums1[m:] = nums2[:n]
         nums1.sort()
